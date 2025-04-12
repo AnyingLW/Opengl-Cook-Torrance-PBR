a8bda23de1d78f849c3344e5b66909af7ce23c0b
AnyingLW <2038470544@qq.com>
2025-04-12 12:35:51 +0800

    assimp

--- Opengl-Cook-Torrance-PBR/main.cpp
+++ Opengl-Cook-Torrance-PBR/main.cpp
@@ -5,6 +5,7 @@
 #include<glm/gtc/matrix_transform.hpp>
 #include<glm/gtc/type_ptr.hpp>
 
+#include"model.h"
 #include"shader.h"
 #include"stb_image.h"
 #include"camera.h"
@@ -46,12 +47,14 @@ int main() {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
+	stbi_set_flip_vertically_on_load(true);
 	glEnable(GL_DEPTH_TEST);
 
 	///////////////////////////////////////////////////////////////////////////////
 
 	Shader lightingShader("shaders/shader.vs", "shaders/shader.fs");
-	Shader lightShader("shaders/lightShader.vs", "shaders/lightShader.fs");
+
+	Model ourModel("resource/backpack/backpack.obj");
 
 	float vertices[] = {
 		// positions          // normals           // texture coords
@@ -115,79 +118,6 @@ int main() {
 	glm::vec3(-4.0f,  2.0f, -12.0f),
 	glm::vec3(0.0f,  0.0f, -3.0f)
 	};
-	//数据->缓冲区
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-	unsigned int lightVAO;
-	glGenVertexArrays(1, &lightVAO);
-	glBindVertexArray(lightVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	//纹理
-	unsigned int diffuseMap = loadTexture("resource/container2.png");
-	unsigned int specularMap = loadTexture("resource/container2_specular.png");
-	lightingShader.use();
-	lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-	lightingShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-	lightingShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-	lightingShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
-	// point light 1
-	lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
-	lightingShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-	lightingShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-	lightingShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-	lightingShader.setFloat("pointLights[0].constant", 1.0f);
-	lightingShader.setFloat("pointLights[0].linear", 0.09f);
-	lightingShader.setFloat("pointLights[0].quadratic", 0.032f);
-	// point light 2
-	lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
-	lightingShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-	lightingShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
-	lightingShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
-	lightingShader.setFloat("pointLights[1].constant", 1.0f);
-	lightingShader.setFloat("pointLights[1].linear", 0.09f);
-	lightingShader.setFloat("pointLights[1].quadratic", 0.032f);
-	// point light 3
-	lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
-	lightingShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-	lightingShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
-	lightingShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
-	lightingShader.setFloat("pointLights[2].constant", 1.0f);
-	lightingShader.setFloat("pointLights[2].linear", 0.09f);
-	lightingShader.setFloat("pointLights[2].quadratic", 0.032f);
-	// point light 4
-	lightingShader.setVec3("pointLights[3].position", pointLightPositions[3]);
-	lightingShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-	lightingShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
-	lightingShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
-	lightingShader.setFloat("pointLights[3].constant", 1.0f);
-	lightingShader.setFloat("pointLights[3].linear", 0.09f);
-	lightingShader.setFloat("pointLights[3].quadratic", 0.032f);
-	// spotLight
-	lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-	lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-	lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-	lightingShader.setFloat("spotLight.constant", 1.0f);
-	lightingShader.setFloat("spotLight.linear", 0.09f);
-	lightingShader.setFloat("spotLight.quadratic", 0.032f);
-	lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-	lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
-	lightingShader.setInt("material.diffuse", 0);
-	lightingShader.setInt("material.specular", 1);
-	lightingShader.setFloat("material.shininess", 32.0f);
 
 	while (!glfwWindowShouldClose(window)) {
 		float currentFrame = (float)glfwGetTime();
@@ -196,44 +126,18 @@ int main() {
 		processInput(window);//处理键盘事件
 		glClearColor(0.5,0.5,0.5,1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//清颜色缓存&深度缓存
-		//激活并绑定不同的纹理
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, specularMap);
 		//物体的shader
 		lightingShader.use();
-		lightingShader.setVec3("spotLight.position", camera.Position);
-		lightingShader.setVec3("spotLight.direction", camera.Front);
-		lightingShader.setVec3("viewPos", camera.Position);
 		glm::mat4 view=camera.GetViewMatrix();
 		lightingShader.setMat4("view", view);
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		lightingShader.setMat4("projection", projection);
-		glBindVertexArray(VAO);
-		for (unsigned int i = 0; i < 10; i++)
-		{
-			glm::mat4 model;
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-			lightingShader.setMat4("model", model);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
-		//灯的shader
-		glm::mat4 model;
-		lightShader.use();
-		lightShader.setMat4("view", view);
-		lightShader.setMat4("projection", projection);
-		for (int i = 0; i < 4; i++) {
-			model = glm::mat4();
-			model = glm::translate(model, pointLightPositions[i]);
-			model = glm::scale(model, glm::vec3(0.2f));
-			lightShader.setMat4("model", model);
-			glBindVertexArray(lightVAO);
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+		lightingShader.setMat4("model", model);
+		ourModel.Draw(lightingShader);
+		
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
