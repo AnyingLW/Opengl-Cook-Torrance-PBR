60f5eb3d02b856289eb07c12d37fc5d8f4af4f8d
AnyingLW <2038470544@qq.com>
2025-04-17 23:08:58 +0800

    泛光

--- Opengl-Cook-Torrance-PBR/shaders/lightShader.fs
+++ Opengl-Cook-Torrance-PBR/shaders/lightShader.fs
@@ -1,20 +1,32 @@
 #version 330 core
-out vec4 color;
+out vec4 FragColor;
 in vec2 TexCoords;
 
-uniform sampler2D hdrBuffer;
-uniform float exposure;
+uniform sampler2D image;
+
+uniform bool horizontal;
+
+uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
 
 void main()
 {             
-    const float gamma = 2.2;
-    vec3 hdrColor = texture(hdrBuffer, TexCoords).rgb;
-
-    // reinhard
-    // vec3 result = hdrColor / (hdrColor + vec3(1.0));
-    // exposure
-    vec3 result = vec3(1.0) - exp(-hdrColor * exposure);
-    // also gamma correct while we're at it       
-    result = pow(result, vec3(1.0 / gamma));
-    color = vec4(result, 1.0f);
+    vec2 tex_offset = 1.0 / textureSize(image, 0); // gets size of single texel
+    vec3 result = texture(image, TexCoords).rgb * weight[0]; // current fragment's contribution
+    if(horizontal)
+    {
+        for(int i = 1; i < 5; ++i)
+        {
+            result += texture(image, TexCoords + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+            result += texture(image, TexCoords - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
+        }
+    }
+    else
+    {
+        for(int i = 1; i < 5; ++i)
+        {
+            result += texture(image, TexCoords + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+            result += texture(image, TexCoords - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
+        }
+    }
+    FragColor = vec4(result, 1.0);
 }
